6b66dcfab5a3ab777b14494d6a77f95f5d572328
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-10-29 08:59:15 +0100

    getting things set up

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+var canvas = document.getElementById("playingField");
+var ctx = canvas.getContext("2d");
+console.log(ctx);
---
+++ scripts/index.ts
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("playingField") as HTMLCanvasElement
+const ctx = canvas.getContext("2d")
+
+console.log(ctx);
